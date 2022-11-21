05152963a0abef7c56bf39208bbc728120c20627
Mellinand Richier Emmanuelle <mellinand.emmanuelle@gmail.com>
2022-11-21 16:03:14 +0100

    routes

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,13 +1,24 @@
 class RobotsController < ApplicationController
-
   def index
     @robots = Robot.all
   end
 
   def new
+    @robot = Robot.new
   end
 
   def create
+    @robot = Robot.new(robot_params)
+    if @robot.save
+      redirect_to robots_path
+    else
+      render "new"
+    end
   end
 
+  private
+
+  def robot_params
+    params.require(:robot).permit(:name, :image, :utility, :description)
+  end
 end
