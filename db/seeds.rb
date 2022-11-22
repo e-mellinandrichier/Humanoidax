c0cb66fc3d05a4072a035f176854dd6a6dab7608
Inès Silvestre <110823086+ines-silvestre@users.noreply.github.com>
2022-11-22 11:59:58 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all # permet de faire une boucle de supprimer/création
+user1 = User.new(email: "toto@gmail.com", password: "blabla")
+user1.save
+
+
+robot1 = Robot.new(name: "Sacha", image: "https://nypost.com/wp-content/uploads/sites/2/2021/12/Ameca_02-1.jpg?quality=90&strip=all", utility: "travaux", description: "Il vous aidera dans vos travaux de la maison")
+robot1.user = user1
+robot1.save
