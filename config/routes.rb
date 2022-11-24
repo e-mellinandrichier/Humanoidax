c769af49c1978e7f85e7e7a84e230f458443d74b
Mellinand Richier Emmanuelle <mellinand.emmanuelle@gmail.com>
2022-11-24 15:42:25 +0100

    2

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,10 @@ Rails.application.routes.draw do
 
   root to: "robots#index"
   resources :robots do
-    resources :bookings, only: [:new, :create, ]
+    resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:destroy, :index, :show, :edit, :update]
-    # resources :bookings, only: [:new, :create]
-    resources :bookings, except: [:destroy, :update, :edit]
-    end
-    resources :bookings, only: [:destroy, :update, :edit]
-  end
+end
   # resources :bookings, only: [:destroy, :index, :show]
 
 
